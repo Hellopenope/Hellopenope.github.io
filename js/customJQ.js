94da55cd8e60e89dff5b50e41b94029ea3c32279
Mike Wroblewski <mike.j.wroblewski@gmail.com>
2015-03-09 17:45:23 -0700

    added smooth scroll jQuery

--- js/customJQ.js
+++ js/customJQ.js
@@ -1,5 +1,14 @@
-$('masthead-nav li').on('click', function(e){
-	e.preventDefault();
-	$('li active').removeClass('active');
-	$(this).addClass('active');
-})
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 1000, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
