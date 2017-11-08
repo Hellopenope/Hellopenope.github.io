359aed09505bffbe8d5ec87ed0bd6ad7992a912e
Mike Wroblewski <mike.j.wroblewski@gmail.com>
2017-11-08 04:36:17 -0800

    Adjust nav link layout, clean up JS and CSS

--- app/javascripts/section_animations.js
+++ app/javascripts/section_animations.js
@@ -2,39 +2,20 @@
 $(function(){
 	// Initial stat of page - click to bring up main content of section
 	$(".landing-nav").click(function(){
+		$('.landing-nav').removeClass('big');
 		var self = $(this),
 				sibs = $(this).siblings();
 
-		populateComp(sibs);
-		$(".landing-nav").slideUp("slow");
+		self.addClass('selected');
+		sibs.removeClass('selected');
+		$(".section").hide();
 		displaySection(self);
-		$("div").promise().done(function() {
-			$(".compressed-sections").slideDown("slow");
-		});
 	});
-	// Populates hidden section links below active section
-	function populateComp(sibs) {
-		var textOne = $(sibs[0]).children().text(),
-				textTwo = $(sibs[1]).children().text();
-		
-		$(".comp-one").text(textOne);
-		$(".comp-two").text(textTwo);
-	};
 	// Animation for section display
 	function displaySection(self) {
 		var targetID = "#"+self.text().replace(/\./g, "");
-		$(targetID).slideDown("slow");
+		$(targetID).show();
 	};
-	// Hidden section - click to activate a different section
-	$(".deep-comp").click(function(){
-		var self = $(this),
-				targetClass = "."+self.text().replace(/\./g, ""),
-				sibs = $(targetClass).siblings();
-
-		$(".section").slideUp("slow");
-		displaySection(self);
-		populateComp(sibs);
-	});
 
 	// Contact links - hover will slide down text of given image/link
 	$(".inner-contact-links").find("img").hover(function(){
